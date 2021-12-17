d0f9bc9bef5100cac170dd7923aa18bc795e9dc9
yiminghe <yiminghe@gmail.com>
2021-12-17 11:01:27 +0800

    https://github.com/microsoft/monaco-editor/pull/2825

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # monaco-editor-2825
+
+https://github.com/microsoft/monaco-editor/pull/2825
+
+```
+# before patch build:
+npm run build
+
+# after patch build
+npm run patched-build
+```
+
+checkout dist and patched-dist dir
---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const P = require('monaco-editor-webpack-plugin');
+const PatchedP = require('./patched-plugin/monaco-editor-webpack-plugin');
+const path = require('path');
+
+const dist = process.env.PATCH ? 'patched-dist' : 'dist';
+const MonacoPlugin = process.env.PATCH ? PatchedP : P;
+
+
+module.exports = {
+  mode: 'production',
+  entry: './index.js',
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+  output: {
+    path: path.resolve(__dirname, dist),
+    filename: 'index.bundle.js',
+  },
+  plugins: [new MonacoPlugin({
+    languages: ['typescript'],
+    features: ['accessibilityHelp'],
+  })],
+};
